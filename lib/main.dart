c755cec02136c05e376b994c70bc468b859285cb
Kimiili <s13302201653@toyo.jp>
2024-07-13 01:12:02 +0900

    Add Fortune cookie to the UI

--- lib/main.dart
+++ lib/main.dart
@@ -53,14 +53,17 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Flutter Demo Home Page"),
-      ),
+      appBar: AppBar(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Image.asset(
+              'assets/images/genre.jpg',
+              width: 200,
+              height: 200,
+              fit: BoxFit.cover,
+              ),
             Card(
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
